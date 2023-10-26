2b5a561a5239011b6fd6062b7adf86fc8b62e611
Kevin Dickey <kdickey@iastate.edu>
2023-10-26 15:52:03 -0500

    workin on updating bubblesort to have nops

--- mips/Proj1_bubblesort.s
+++ mips/Proj1_bubblesort.s
@@ -19,8 +19,8 @@ main:
 #li $v0, 5
 #syscall
 
-addi $s0, $zero, 0	# i = 0
-addi $s1, $zero, 0	# j = 0
+#addi $s0, $zero, 0	# i = 0		# commented out b/c do it anyway on line 50
+addi $s1, $zero, 0	# j = 0		# was commented out b/c do it anyway on line 51, may want to keep
 addi $s2, $zero, 10	# N = 10 (num elements in array)
 la $s3, array		# s3 = array base address
 
@@ -48,11 +48,10 @@ readArrayElements:
 # at this point all elements should be properly in array
 bubbleSort:
 addi $s0, $zero, 0 	# i = 0 (resetting i after using it to enter array elements)
-addi $t0, $s2, -1	# t0 = N - 1
-# may need to reset all temp registers here? not sure
+addi $t0, $s2, -1	# t0 = N - 1		# will be ok with forwarding result from ALU, otherwise needs 1 nop before this instruc
 outerCond:
 addi $s1, $zero, 0	# j = 0
-slt $t1, $s0, $t0	# t1 = 1 when i < N-1
+slt $t1, $s0, $t0	# t1 = 1 when i < N-1 	# will be ok with forwarding result from ALU, otherwise needs 2 nop before this instruc
 beq $t1, $zero, printResults
 addi $t2, $zero, 0	# t2 acts as a boolean, t2 = 0 = false, see "https://www.geeksforgeeks.org/bubble-sort/" for more info on this part
 innerCond:
